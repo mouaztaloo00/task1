e534ef3784aee1759b008f577776170fcb771ba1
mouaztaloo00 <moaztalo14@gmail.com>
2024-09-01 12:38:31 +0300

    yap

--- src/App.js
+++ src/App.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import Navbar from './Components/Navbar'
-import Home from './Pages/Home'
-import Hometest from './Pages/Hometest'
-
-import Update from './Pages/Update'
-import Delete from './Pages/Delete'
-import SignUp from './Components/SignUp'
-import Login from './Components/Login'
-import { Routes , Route } from 'react-router-dom'
-
-function App() {
-  return (
-   <>
-   <Navbar/>
-   <div className='container'>
-    <Routes>
-      <Route path='/' element={<Login/>}/>
-      <Route path='/Sign-Up' element={<SignUp/>}/> 
-      <Route path='/Home' element={<Home/>}/>
-      <Route path='/Hometest' element={<Hometest/>}/>
-
-      <Route path='/update' element={<Update/>}/>
-      <Route path='/delete' element={<Delete/>}/>
-    </Routes>
-   </div>
-   </>
-=======
 import React from 'react';
 import Navbar from './Components/Navbar';
 import Home from './Pages/Home';
@@ -53,7 +25,6 @@ function App() {
         </Routes>
       </div>
     </Router>
->>>>>>> 992d90bfd6f2f538920dd2cf08e7d4e3c7d7e97c
   );
 }
 
